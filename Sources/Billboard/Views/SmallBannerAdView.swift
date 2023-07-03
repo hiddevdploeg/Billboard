3ad62f04696836e8a3d715e5a2a4bbedc4e3423c
Hidde van der Ploeg <hiddevanderploeg@me.com>
2023-07-03 13:43:41 +0200

    Added shadow to small banner view

--- Sources/Billboard/Views/SmallBannerAdView.swift
+++ Sources/Billboard/Views/SmallBannerAdView.swift
@@ -13,14 +13,16 @@ public struct SmallBannerAdView : View {
     
     let advert : BillboardAd
     let config : BillboardConfiguration
+    let includeShadow : Bool
     
     @State private var canDismiss = false
     @State private var appIcon : UIImage? = nil
     @State private var showAdvertisement = true
     
-    public init(advert: BillboardAd, config: BillboardConfiguration = BillboardConfiguration()) {
+    public init(advert: BillboardAd, config: BillboardConfiguration = BillboardConfiguration(), includeShadow: Bool = true) {
         self.advert = advert
         self.config = config
+        self.includeShadow = includeShadow
     }
     
     public var body: some View {
@@ -86,9 +88,8 @@ public struct SmallBannerAdView : View {
             
         }
         .padding(10)
-        .background(advert.background)
-        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
-        
+        .background(backgroundView)
+        .overlay(RoundedRectangle(cornerRadius: 16, style: .continuous).stroke(Color.primary.opacity(0.1), lineWidth: 1))
         .task {
             if let data = try? await advert.getAppIcon() {
                 appIcon = UIImage(data: data)
@@ -102,6 +103,15 @@ public struct SmallBannerAdView : View {
         }
         
     }
+    
+
+    var backgroundView : some View {
+        RoundedRectangle(cornerRadius: 16, style: .continuous)
+            .fill(advert.background)
+            .shadow(color: includeShadow ? advert.background.opacity(0.5) : Color.clear,
+                    radius: 6,
+                    x: 0, y: 2)
+    }
 }
 
 
@@ -109,7 +119,7 @@ struct SmallBannerAdView_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
             SmallBannerAdView(advert: BillboardSamples.sampleDefaultAd)
-            SmallBannerAdView(advert: BillboardSamples.sampleDefaultAd)
+            SmallBannerAdView(advert: BillboardSamples.sampleDefaultAd, includeShadow: false)
         }
         .padding()
         
