6783259a4f17b723c0dff1e228e66c73252760a2
John Scott <john@jjrscott.com>
2023-09-01 00:31:45 +0100

    Fix BillboardExample build failure

--- Sources/Billboard/Utilities/Font+iOS15.swift
+++ Sources/Billboard/Utilities/Font+iOS15.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 extension Font {
-    static func compatibleSystem(_ style: TextStyle, design: Design?, weight: Weight?) -> Font {
+    public static func compatibleSystem(_ style: TextStyle, design: Design?, weight: Weight?) -> Font {
         if #available(iOS 16.0, *) {
             return .system(style, design: design, weight: weight)
         } else {
