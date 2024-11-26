222d51618aa9a39ca0096eeceff5151b733822fb
Hidde van der Ploeg <hiddevanderploeg@me.com>
2024-11-26 17:18:09 +0000

    Moved files out of folder and updated countdown for tvOS

--- Sources/Views/BillboardCountdownView.swift
+++ Sources/Views/BillboardCountdownView.swift
@@ -20,6 +20,43 @@ struct BillboardCountdownView : View {
     private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
+        #if os(tvOS)
+        ZStack {
+            Circle()
+                .stroke(advert.tint.opacity(0.2), style: StrokeStyle(lineWidth: 6, lineCap: .round, lineJoin: .round))
+            
+            Circle()
+                .trim(from: 0, to: timerProgress)
+                .stroke(advert.tint, style: StrokeStyle(lineWidth: 6, lineCap: .round, lineJoin: .round))
+            
+            Text("\(seconds, specifier: "%.0f")")
+                .font(.system(.caption, design: .rounded, weight: .heavy).monospacedDigit())
+                .rotationEffect(.degrees(90))
+                .minimumScaleFactor(0.5)
+                .animation(.default, value: seconds)
+                .transition(.identity)
+                .contentTransition(.numericText(value: seconds))
+                .onReceive(timer) { _ in
+                    if seconds > 0 {
+                        seconds -= 1
+                    }
+                }
+        }
+        .foregroundColor(advert.tint)
+        .rotationEffect(.degrees(-90))
+        .frame(width: 64, height: 64)
+        .onAppear {
+            seconds = totalDuration
+            withAnimation(.linear(duration: totalDuration)) {
+                timerProgress = 1.0
+            }
+        }
+        .onChange(of: seconds, {
+            if seconds < 1 {
+                canDismiss = true
+            }
+        })
+        #else
         ZStack {
             Circle()
                 .stroke(advert.tint.opacity(0.2), style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
@@ -66,6 +103,7 @@ struct BillboardCountdownView : View {
             canDismiss = true
             #endif
         }
+        #endif
     }
 }
 
